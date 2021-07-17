43cacdebf444427077fdc11d03f0a362d8f2ddaf
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-07-17 23:09:24 +0530

    Add files via upload

--- module4-assignment/script.js
+++ module4-assignment/script.js
@@ -3,7 +3,7 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (name in names) {
-  if (name.charAt(0) == "j" or name.charAt(0) == "J") {
+  if (name.charAt(0) == "j") {
     byeSpeaker.speak(name);
   } else {
     helloSpeaker.speak(name);
