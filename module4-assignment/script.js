09408106bee401f5581c969a5038c60b978aa37a
Shreya Jha <63197905+Shreya19595@users.noreply.github.com>
2021-07-17 23:01:38 +0530

    Add files via upload

---
+++ module4-assignment/script.js
@@ -0,0 +1,11 @@
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (name in names) {
+  if (name.charAt(0) == "j" or name.charAt(0) == "J") {
+    byeSpeaker.speak(name);
+  } else {
+    helloSpeaker.speak(name);
+  }
+}();
